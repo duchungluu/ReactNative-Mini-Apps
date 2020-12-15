e3ede271647eb6de25556a82c5faf85ef203db32
Hung Luu <duchung.luu@live.com>
2020-12-15 14:04:40 +0200

    Add more to app

--- mightyapp/src/screens/ListScreen.js
+++ mightyapp/src/screens/ListScreen.js
@@ -3,9 +3,9 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friend #1', age: 20 },
+    { name: 'Friend #1', age: 12 },
     { name: 'Friend #2', age: 21 },
-    { name: 'Friend #3', age: 22 },
+    { name: 'Friend #3', age: 21 },
     { name: 'Friend #4', age: 23 },
     { name: 'Friend #5', age: 24 },
     { name: 'Friend #6', age: 25 },
@@ -16,6 +16,8 @@ const ListScreen = () => {
 
   return (
     <FlatList
+      //horizontal
+      showsHorizontalScrollIndicator={true}
       keyExtractor={friend => friend.name}
       data={friends}
       renderItem={({ item }) => {
