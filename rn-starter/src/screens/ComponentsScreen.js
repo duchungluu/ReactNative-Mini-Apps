8ec0ce737611a84ca7e6577bb460ba2b9bb934f4
Hung Luu <duchung.luu@live.com>
2020-12-15 10:47:12 +0200

    MOre changes cause I can

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  const name = 'Louis';
+  const name = 'Stephen';
 
   return (
     <View>
